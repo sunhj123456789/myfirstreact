e7926a20961eb2d6a44910d7ad3d413a70a118b4
sundar <sunhj123456789@gmail.com>
2021-05-14 20:54:35 +0530

    useState hook

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 function ExpenstionItem(props) {
+  const [title, setTitle] = useState(props.title);
+  //const data = useState(props.title);
+
+  const changeTitle = () => {
+    console.log("clicked");
+    setTitle("we updated the Title");
+  };
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
+        <button type="button" onClick={changeTitle}>
+          Change the Title
+        </button>
       </div>
     </div>
   );
